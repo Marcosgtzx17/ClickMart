d1b32d17681d433f551b78c4eb69f987bdb7b380
Marcos Vladimir Calzadilla Gutiérrez <cg25003@esfe.agape.edu.sv>
2025-09-09 15:21:35 -0600

    Se creó la entidad Resena

---
+++ ClickMart/Entidades/Resena.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ClickMart.Entidades
+{
+    public class Resena
+    {
+        [Key]
+        public int RESENA_ID { get; set; }
+
+        [Required]
+        public int USUARIO_ID { get; set; }
+
+        [Required]
+        public int PRODUCTO_ID { get; set; }
+
+        [Range(1, 5)]
+        public int calificacion { get; set; }
+
+        [MaxLength(1000)]
+        public string comentario { get; set; }
+
+        public DateTime fecha_resena { get; set; }
+    }
+}
