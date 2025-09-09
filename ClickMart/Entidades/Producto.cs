13c47db339871314a94a9f0c06d0e8b64f96c3bd
Henry <jm25002@esfe.agape.edu.sv>
2025-09-09 15:05:23 -0600

    Añadir clase Producto con propiedades y relaciones
    Se ha creado la clase `Producto` en `Producto.cs`, que incluye propiedades como `ProductoId`, `Nombre`, `Descripcion`, `Talla`, `Precio`, `Marca` y `Stock`. También se han establecido relaciones con `CategoriaProducto` y `Distribuidor`, utilizando Data Annotations para definir restricciones y características de las propiedades.

---
+++ ClickMart/Entidades/Producto.cs
@@ -0,0 +1,51 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ClickMart.Entidades
+{
+    [Table("productos")]
+    public class Producto
+    {
+        [Key]
+        [Column("PRODUCTO_ID")]
+        public int ProductoId { get; set; }
+
+        [Required]
+        [Column("NOMBRE")]
+        [StringLength(50)]
+        public string Nombre { get; set; } = string.Empty;
+
+        [Column("DESCRIPCION")]
+        [StringLength(100)]
+        public string? Descripcion { get; set; }
+
+        [Column("TALLA")]
+        [StringLength(10)]
+        public string? Talla { get; set; }
+
+        [Required]
+        [Column("PRECIO", TypeName = "decimal(10,2)")]
+        public decimal Precio { get; set; }
+
+        [Column("MARCA")]
+        [StringLength(30)]
+        public string? Marca { get; set; }
+
+        [Column("STOCK")]
+        public int? Stock { get; set; }
+
+        // ======= Relaciones =======
+
+        [Column("ID_CATEGORIA")]
+        public int? CategoriaId { get; set; }
+
+        [ForeignKey("CategoriaId")]
+        public CategoriaProducto? Categoria { get; set; }
+
+        [Column("ID_DISTRIBUIDOR")]
+        public int? DistribuidorId { get; set; }
+
+        [ForeignKey("DistribuidorId")]
+        public Distribuidor? Distribuidor { get; set; }
+    }
+}
