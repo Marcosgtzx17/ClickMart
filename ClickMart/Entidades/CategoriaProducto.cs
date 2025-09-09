5d3ae3ea1c7b7e236ebed1f6fb0da0f42ec245b5
Henry <jm25002@esfe.agape.edu.sv>
2025-09-09 15:00:20 -0600

    Añadir clase CategoriaProducto con validaciones
    Se han añadido directivas `using` para la validación de datos y la configuración de la base de datos. Se ha creado la clase `CategoriaProducto` en el espacio de nombres `ClickMart.Entidades`, con atributos para mapear a la tabla `categoria_productos`. La clase incluye propiedades `CategoriaId` como clave primaria y `Nombre` como campo obligatorio con longitud máxima de 120 caracteres.

---
+++ ClickMart/Entidades/CategoriaProducto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ClickMart.Entidades
+{
+    [Table("categoria_productos")]
+    public class CategoriaProducto
+    {
+        [Key]
+        [Column("CATEGORIA_ID")]
+        public int CategoriaId { get; set; }
+
+        [Required]
+        [MaxLength(120)]
+        [Column("NOMBRE")]
+        public string Nombre { get; set; } = string.Empty;
+    }
+}
