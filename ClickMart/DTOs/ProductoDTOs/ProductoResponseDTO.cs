962f424d79ddcf4e0c5ff08360a9bc08d1f0e738
Henry <jm25002@esfe.agape.edu.sv>
2025-09-10 14:25:52 -0600

    Añadir clases DTO para productos
    Se han creado las clases `ProductoCreateDTO`, `ProductoResponseDTO` y `ProductoUpdateDTO` en el espacio de nombres `ClickMart.DTOs.ProductoDTOs`. Estas clases incluyen propiedades para representar los atributos de un producto, como `Nombre`, `Descripcion`, `Talla`, `Precio`, `Marca`, `Stock`, `CategoriaId` y `DistribuidorId`, con tipos adecuados y valores por defecto.

---
+++ ClickMart/DTOs/ProductoDTOs/ProductoResponseDTO.cs
@@ -0,0 +1,15 @@
+﻿namespace ClickMart.DTOs.ProductoDTOs
+{
+    public class ProductoResponseDTO
+    {
+        public int ProductoId { get; set; }
+        public string Nombre { get; set; } = string.Empty;
+        public string? Descripcion { get; set; }
+        public string? Talla { get; set; }
+        public decimal Precio { get; set; }
+        public string? Marca { get; set; }
+        public int? Stock { get; set; }
+        public int? CategoriaId { get; set; }
+        public int? DistribuidorId { get; set; }
+    }
+}
