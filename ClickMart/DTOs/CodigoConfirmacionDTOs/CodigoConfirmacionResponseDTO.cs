f3b817ef9c5524a9842ba5106bdf06371cfc9871
Marcos Vladimir Calzadilla Gutiérrez <cg25003@esfe.agape.edu.sv>
2025-09-10 01:09:53 -0600

    Se creó la clase codigo de confirmación.

---
+++ ClickMart/DTOs/CodigoConfirmacionDTOs/CodigoConfirmacionResponseDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace ClickMart.DTOs.CodigoConfirmacionDTOs
+{
+    public class CodigoConfirmacionResponseDTO
+    {
+        public int IdCodigo { get; set; }
+        public string Email { get; set; } = string.Empty;
+        public string Codigo { get; set; } = string.Empty;
+        public DateTime FechaGeneracion { get; set; }
+        public int Usado { get; set; }
+    }
+}
