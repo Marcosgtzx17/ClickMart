8cc835c0c11f498a56ea20f4d56e4c067dd20695
cindy hernandez <hr25001@esfe.agape.edu.sv>
2025-09-10 01:53:52 -0600

    Se creo DTOs de Rol

---
+++ ClickMart/DTOs/RolDTOs/RolResponseDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace ClickMart.DTOs.RolDTOs
+{
+    namespace ClickMart.DTOs.RolDTOs
+    {
+        public class RolResponseDTO
+        {
+            public int RolId { get; set; }
+            public string Nombre { get; set; } = string.Empty;
+        }
+    }
+}
