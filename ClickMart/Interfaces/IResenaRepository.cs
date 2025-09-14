473f4b96a0a9496404d1f4d4d07fbfb41db80555
Cindy Hernandez <hr25001@esfe.agape.edu.sv>
2025-09-14 16:25:11 -0600

    Añadir interfaces IResenaRepository e IResenaService
    Se ha implementado la interfaz `IResenaRepository` para gestionar operaciones asíncronas de reseñas, incluyendo métodos para obtener, agregar, actualizar y eliminar reseñas.
    
    Además, se ha añadido la interfaz `IResenaService`, que permite manejar operaciones relacionadas con las reseñas utilizando DTOs para la creación, actualización y eliminación de reseñas.

---
+++ ClickMart/Interfaces/IResenaRepository.cs
@@ -0,0 +1,14 @@
+﻿using ClickMart.Entidades;
+using ClickMart.Interfaces;
+
+namespace ClickMart.Interfaces
+{
+    public interface IResenaRepository
+    {
+        Task<List<Resena>> GetAllAsync();
+        Task<Resena?> GetByIdAsync(int id);
+        Task<Resena> AddAsync(Resena entity);
+        Task<bool> UpdateAsync(Resena entity);
+        Task<bool> DeleteAsync(int id);
+    }
+}
---
+++ ClickMart/Interfaces/IResenaService.cs
@@ -0,0 +1,13 @@
+﻿using ClickMart.DTOs.ResenaDTOs;
+
+namespace ClickMart.Interfaces
+{
+    public interface IResenaService
+    {
+        Task<List<ResenaResponseDTO>> GetAllAsync();
+        Task<ResenaResponseDTO?> GetByIdAsync(int id);
+        Task<ResenaResponseDTO> CreateAsync(ResenaCreateDTO dto);
+        Task<bool> UpdateAsync(int id, ResenaUpdateDTO dto);
+        Task<bool> DeleteAsync(int id);
+    }
+}
