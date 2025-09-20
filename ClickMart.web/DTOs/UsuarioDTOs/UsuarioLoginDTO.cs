8ef6b087516a5e71a74de7cd0f9d0d2967bc5154
Kevin José Mendoza Tobar <mt25001@esfe.agape.edu.sv>
2025-09-19 23:25:19 -0600

    Se creo el DTO usuario login frot end

---
+++ ClickMart.web/DTOs/UsuarioDTOs/UsuarioLoginDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace ClickMart.web.DTOs.UsuarioDTOs
+{
+    public class UsuarioLoginDTO
+    {
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
