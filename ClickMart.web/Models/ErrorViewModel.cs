2fb834e53fc60905bb82636c0ac0f0051ade396b
Kevin José Mendoza Tobar <mt25001@esfe.agape.edu.sv>
2025-09-19 21:31:41 -0600

    Se agrega el front end al proyecto

---
+++ ClickMart.web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ClickMart.web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
