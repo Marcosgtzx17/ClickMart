f327ee86fd8861a971a04dfd6665ed164637dfcb
Kevin José Mendoza Tobar <mt25001@esfe.agape.edu.sv>
2025-09-26 14:58:34 -0600

    modificacion del PDF para despues utilizarlo en front end

--- ClickMart/DTOs/FacturaDTOs/FacturaItemDTO.cs
+++ ClickMart/DTOs/FacturaDTOs/FacturaItemDTO.cs
@@ -6,5 +6,8 @@
         public int Cantidad { get; set; }
         public decimal PrecioUnitario { get; set; }
         public decimal Subtotal { get; set; }
+
+        // NUEVO: thumbnail del producto (puede ser null)
+        public byte[]? ImagenProducto { get; set; }
     }
 }
